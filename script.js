d4fae1f6038c56577380b1579f31efdf7adddc97
ne_mene21 <73829851+nemene21@users.noreply.github.com>
2023-02-14 12:21:04 +0100

    many things
    role choosing, button animations, role adding, style

--- script.js
+++ script.js
@@ -1,11 +1,35 @@
 
 let body = document.getElementsByTagName("body")[0];
+let roles = [];
 
 function new_role() {
-    let name = prompt("Role name?");
+    let name = prompt("Ime uloge?", "Nevin");
+    if (name == null) return
+
+    let number = prompt("Količina?", "1");
+    if (number == null) return
 
     let paragraph = document.createElement("p");
-    paragraph.textContent = name;
+    paragraph.textContent = name + ": " + number;
+    paragraph.classList.add("role-text");
+
+    for (let i = 0; i < Number(number); i++) {
+        roles.push(name);
+    }
 
     body.appendChild(paragraph);
+}
+
+function choose_roles() {
+    roles.sort(() => Math.random() - 0.5);
+
+    for (let i = 0; i < roles.length; i++) {
+        alert("Ti si " + roles[i] + "!");
+
+        if (i == roles.length - 1) {
+            alert("Gotovo...");
+        } else {
+            alert("Dalje...");
+        }
+    }
 }
